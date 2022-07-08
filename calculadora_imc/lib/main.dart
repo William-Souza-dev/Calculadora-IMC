9f7afbb4143bbd209481aebdc0bcf61804f6af73
William Souza <Will051096@gmail.com>
2022-07-07 21:41:11 -0300

    Concluido todas as Funções dos botões

--- calculadora_imc/lib/main.dart
+++ calculadora_imc/lib/main.dart
@@ -18,13 +18,38 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   TextEditingController pesoController = TextEditingController();
   TextEditingController alturaController = TextEditingController();
+  GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
   String _infoText = "Informe seus dados!";
 
   void _resetar() {
     pesoController.text = "";
     alturaController.text = "";
-    _infoText = "Informe seus dados!";
+    setState(() {
+      _infoText = "Informe seus dados!";
+      _formKey = GlobalKey<FormState>();
+    });
+  }
+
+  void _calcular() {
+    setState(() {
+      double peso = double.parse(pesoController.text);
+      double altura = double.parse(alturaController.text) / 100;
+      double imc = peso / (altura * altura);
+      if (imc < 18.6) {
+        _infoText = "Abaixo do Peso (${imc.toStringAsPrecision(3)})";
+      } else if (imc >= 18.6 && imc < 24.9) {
+        _infoText = "Peso Ideal (${imc.toStringAsPrecision(3)})";
+      } else if (imc >= 25 && imc < 29.9) {
+        _infoText = "Levemente Acima do Peso (${imc.toStringAsPrecision(3)})";
+      } else if (imc >= 30 && imc < 34.9) {
+        _infoText = "Obesidade Grau I (${imc.toStringAsPrecision(3)})";
+      } else if (imc >= 35 && imc < 39.9) {
+        _infoText = "Obesidade Grau II (${imc.toStringAsPrecision(3)})";
+      } else if (imc >= 40) {
+        _infoText = "Obesidade Grau III (${imc.toStringAsPrecision(3)})";
+      }
+    });
   }
 
   @override
@@ -44,53 +69,71 @@ class _HomeState extends State<Home> {
       backgroundColor: Colors.white,
       body: SingleChildScrollView(
         padding: const EdgeInsets.fromLTRB(10.0, 0.0, 10.0, 0.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            const Icon(Icons.person_outline, size: 120.0, color: Colors.green),
-            TextField(
-              keyboardType: TextInputType.number,
-              decoration: const InputDecoration(
-                labelText: "Peso (kg)",
-                labelStyle: TextStyle(color: Colors.green),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              const Icon(Icons.person_outline,
+                  size: 120.0, color: Colors.green),
+              TextFormField(
+                keyboardType: TextInputType.phone,
+                decoration: const InputDecoration(
+                  labelText: "Peso (kg)",
+                  labelStyle: TextStyle(color: Colors.green),
+                ),
+                textAlign: TextAlign.center,
+                style: const TextStyle(color: Colors.green, fontSize: 25.0),
+                controller: pesoController,
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return "Insira seu Peso!";
+                  }
+                },
               ),
-              textAlign: TextAlign.center,
-              style: const TextStyle(color: Colors.green, fontSize: 25.0),
-              controller: pesoController,
-            ),
-            TextField(
-              keyboardType: TextInputType.number,
-              decoration: const InputDecoration(
-                labelText: "Altura (cm)",
-                labelStyle: TextStyle(color: Colors.green),
+              TextFormField(
+                keyboardType: TextInputType.phone,
+                decoration: const InputDecoration(
+                  labelText: "Altura (cm)",
+                  labelStyle: TextStyle(color: Colors.green),
+                ),
+                textAlign: TextAlign.center,
+                style: const TextStyle(color: Colors.green, fontSize: 25.0),
+                controller: alturaController,
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return "Insira sua Altura!";
+                  }
+                },
               ),
-              textAlign: TextAlign.center,
-              style: const TextStyle(color: Colors.green, fontSize: 25.0),
-              controller: alturaController,
-            ),
-            Padding(
-              padding: const EdgeInsets.only(top: 10, bottom: 10),
-              child: SizedBox(
-                height: 50.0,
-                child: ElevatedButton(
-                  onPressed: () {},
-                  child: const Text("Calcular"),
-                  style: ElevatedButton.styleFrom(
-                    primary: Colors.green,
-                    textStyle: const TextStyle(
-                      color: Colors.white,
-                      fontSize: 25.0,
+              Padding(
+                padding: const EdgeInsets.only(top: 10, bottom: 10),
+                child: SizedBox(
+                  height: 50.0,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      if (_formKey.currentState!.validate()) {
+                        _calcular();
+                      }
+                    },
+                    child: const Text("Calcular"),
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.green,
+                      textStyle: const TextStyle(
+                        color: Colors.white,
+                        fontSize: 25.0,
+                      ),
                     ),
                   ),
                 ),
               ),
-            ),
-            Text(
-              _infoText,
-              textAlign: TextAlign.center,
-              style: TextStyle(color: Colors.green, fontSize: 25.0),
-            ),
-          ],
+              Text(
+                _infoText,
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.green, fontSize: 25.0),
+              ),
+            ],
+          ),
         ),
       ),
     );
